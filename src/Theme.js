ec07c57c00785caab692af81571600a5ede1f3d3
lukaszkieronski <30519352+lukaszkieronski@users.noreply.github.com>
2022-08-11 12:18:59 +0200

    theme colors replaced with material definition

--- src/Theme.js
+++ src/Theme.js
@@ -1,16 +1,13 @@
 import { createTheme } from "@mui/material";
+import { blueGrey } from '@mui/material/colors'
+
+
 
 export const ApplicationTheme = createTheme({
     palette: {
         type: 'light',
-        primary: {
-            main: '#3f51b5',
-        },
-        secondary: {
-            main: '#f50057',
-        },
         background: {
-            default: '#cfd8dc',
+            default: blueGrey[200],
         },
     }
 })
