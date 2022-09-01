20facf8f1872b8c9e5a8be10ab58d69bbc58501e
lukaszkieronski <30519352+lukaszkieronski@users.noreply.github.com>
2022-09-01 11:18:14 +0200

    refactor continued

--- src/components/Questions/Question.jsx
+++ src/components/Questions/Question.jsx
@@ -1,9 +1,14 @@
-import { Button, Dialog, DialogContent, DialogContentText, DialogTitle, Divider, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from "@mui/material"
-import { useState } from "react";
+import { Button, Dialog, DialogContent, DialogContentText, DialogTitle, Divider, FormControlLabel, Radio, RadioGroup } from "@mui/material"
+import { useState, useMemo } from "react";
+import { questions } from 'data/questions'
 
 export const Question = ({ questionId, handleClose }) => {
 
     const [answer, setAnswer] = useState(0)
+
+    const questionData = useMemo(() => {
+        return questionId in questions ? questions[questionId] : undefined
+    }, [questionId])
 
     const tryClose = (event, reason) => {
         if (reason && reason == "backdropClick")
@@ -17,15 +22,14 @@ export const Question = ({ questionId, handleClose }) => {
             <DialogTitle>Pytanie {questionId}</DialogTitle>
             <DialogContent >
                 <DialogContentText>
-                    To subscribe to this website, please enter your email address here. We
-                    will send updates occasionally.
+                    {questionData?.text}
                 </DialogContentText>
+                {questionData?.img && <img src={questionData.img} width="100%" />}
                 <Divider sx={{ mt: 2, mb: 2 }} />
                 <RadioGroup value={answer} onChange={(event) => setAnswer(event.target.value)}>
-                    <FormControlLabel value={1} control={<Radio />} label="odpowiedź 1" />
-                    <FormControlLabel value={2} control={<Radio />} label="odpowiedź 2" />
-                    <FormControlLabel value={3} control={<Radio />} label="odpowiedź 3" />
-                    <FormControlLabel value={4} control={<Radio />} label="odpowiedź 4" />
+                    <FormControlLabel value={1} control={<Radio />} label={questionData?.answers[0]} />
+                    <FormControlLabel value={2} control={<Radio />} label={questionData?.answers[1]} />
+                    <FormControlLabel value={3} control={<Radio />} label={questionData?.answers[2]} />
                 </RadioGroup>
                 <Divider sx={{ mt: 2, mb: 2 }} />
                 <Button variant="contained" onClick={tryClose} disabled={answer == 0}>Odpowiedz</Button>
--- src/data/questions.js
+++ src/data/questions.js
@@ -1,11 +1,11 @@
 export const questions = {
-    1: { id: 1, text: "Pytanie 1", triggerOn: 1, answers: ["1", "2", "3", "4"] },
-    2: { id: 2, text: "Pytanie 2", triggerOn: 1, answers: ["1", "2", "3", "4"] },
-    3: { id: 3, text: "Pytanie 3", triggerOn: 2, answers: ["1", "2", "3", "4"] },
-    4: { id: 4, text: "Pytanie 4", triggerOn: 2, answers: ["1", "2", "3", "4"] },
-    5: { id: 5, text: "Pytanie 5", triggerOn: 3, answers: ["1", "2", "3", "4"] },
-    6: { id: 6, text: "Pytanie 6", triggerOn: 3, answers: ["1", "2", "3", "4"] },
-    7: { id: 7, text: "Pytanie 7", triggerOn: 4, answers: ["1", "2", "3", "4"] },
+    1: { id: 1, text: "Pytanie 1", triggerOn: 1, answers: ["1", "2", "3"] },
+    2: { id: 2, text: "Pytanie 2", triggerOn: 1, answers: ["1", "2", "3"] },
+    3: { id: 3, text: "Pytanie 3", triggerOn: 2, answers: ["1", "2", "3"] },
+    4: { id: 4, text: "Pytanie 4", triggerOn: 2, answers: ["1", "2", "3"] },
+    5: { id: 5, text: "Pytanie 5", triggerOn: 3, answers: ["1", "2", "3"] },
+    6: { id: 6, text: "Pytanie 6", triggerOn: 3, answers: ["1", "2", "3"] },
+    7: { id: 7, text: "Pytanie 7", triggerOn: 4, answers: ["1", "2", "3"] },
 }
 
 export const questionList = Object.values(questions)
